3bd48144c5404f9e174763a2c3f7926df33d3807
Matthew Maguire <mhmaguire0@gmail.com>
2024-08-23 18:37:44 -0700

    type hints and comments for web driver interface classes

--- auto_resume/page/element.py
+++ auto_resume/page/element.py
@@ -1,60 +1,124 @@
 import types
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.by import By
+from typing import Union, Callable, Type, Optional, Protocol, Tuple
 
+class HasDriver(Protocol):
+    driver: WebDriver
+
+Locator = Tuple[By, str]
 
 class Element:
+    def __init__(self, locator: Locator, many: bool = True) -> None:
+        """
+        Initialize the Element object with a locator and a flag indicating if multiple elements are expected.
 
-    def __init__(self, locator, many=True):
+        :param locator: Locator tuple (By, value) to find the element(s).
+        :param many: Boolean flag indicating if multiple elements are expected.
+        """
         self.locator = locator
         self.is_many = many
         self.timeout = 1
 
-    def __set_name__(self, owner, name):
+    def __set_name__(self, owner: Type, name: str) -> None:
+        """
+        Set the name of the attribute on the owner class.
+
+        :param owner: The owner class where the attribute is defined.
+        :param name: The name of the attribute.
+        """
         self.__name__ = name
 
-    def __call__(self, instance, timeout=10, reraise=False):
-        try: 
-            return instance.wait_for(self.present(), timeout=timeout)
-        
+    def __call__(self, instance: HasDriver, timeout: int = 10, reraise: bool = False) -> Union[bool, WebElement, list[WebElement]]:
+        """
+        Call the element, waiting for its presence and optionally raising exceptions.
+
+        :param instance: The instance of the class where the element is defined.
+        :param timeout: Maximum time to wait for the element.
+        :param reraise: Boolean flag to indicate if exceptions should be re-raised.
+        :return: The WebElement(s) if found, otherwise False.
+        """
+        try:
+            return instance.driver.wait_for(self.present(), timeout=timeout)
         except (NoSuchElementException, TimeoutException) as e:
             if reraise:
                 raise e
-
             return False
-            
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance: Optional[HasDriver], owner: Type) -> Union[Callable, Locator]:
+        """
+        Get the element, returning a method if accessed through an instance, or the locator if accessed through the class.
+        Uses the descriptor protocol to lazily get the fresh driver from the host object.
+
+        :param instance: The instance of the class where the element is defined.
+        :param owner: The owner class where the attribute is defined.
+        :return: A method bound to the instance or the locator tuple.
+        """
         if instance is None:
             return self.locator
-
         return types.MethodType(self, instance)
 
-    def find(self, driver):
+    def find(self, driver: WebDriver) -> Union[WebElement, list[WebElement]]:
+        """
+        Find the element(s) using the provided WebDriver.
+
+        :param driver: WebDriver instance to interact with the web page.
+        :return: The WebElement(s) found by the locator.
+        """
         if self.is_many:
             return driver.find_elements(*self.locator)
         else:
             return driver.find_element(*self.locator)
 
-    def visible(self):
+    def visible(self) -> Callable:
+        """
+        Get the expected condition for the visibility of the element(s).
+
+        :return: The expected condition for visibility.
+        """
         if self.is_many:
             return EC.visibility_of_all_elements_located(self.locator)
         else:
             return EC.visibility_of_element_located(self.locator)
 
-    def present(self):
+    def present(self) -> Callable:
+        """
+        Get the expected condition for the presence of the element(s).
+
+        :return: The expected condition for presence.
+        """
         if self.is_many:
             return EC.presence_of_all_elements_located(self.locator)
         else:
             return EC.presence_of_element_located(self.locator)
 
-    def clickable(self):
+    def clickable(self) -> Callable:
+        """
+        Get the expected condition for the element to be clickable.
+
+        :return: The expected condition for clickability.
+        """
         return EC.element_to_be_clickable(self.locator)
 
     @classmethod
-    def one(cls, locator):
+    def one(cls, locator: Locator) -> "Element":
+        """
+        Create an Element instance for a single element.
+
+        :param locator: Locator tuple (By, value) to find the element.
+        :return: An Element instance configured for a single element.
+        """
         return cls(locator, many=False)
 
     @classmethod
-    def many(cls, locator):
+    def many(cls, locator: Locator) -> "Element":
+        """
+        Create an Element instance for multiple elements.
+
+        :param locator: Locator tuple (By, value) to find the elements.
+        :return: An Element instance configured for multiple elements.
+        """
         return cls(locator, many=True)
--- auto_resume/page/page.py
+++ auto_resume/page/page.py
@@ -1,19 +1,55 @@
+from selenium import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from selenium.webdriver.common.by import By
+from typing import Callable, TypeVar, Union
+from selenium.webdriver.remote.webelement import WebElement
+
+
+T = TypeVar('T')
+
 
 class Page:
-    def __init__(self, driver=None):
+    def __init__(self, driver: WebDriver) -> None:
+        """
+        Initialize the Page object with a WebDriver instance.
+
+        :param driver: WebDriver instance to interact with the web page.
+        """
         self.driver = driver
 
-    def await_url(self, url):
+    def await_url(self, url: str) -> None:
+        """
+        Wait until the URL contains the specified substring.
+
+        :param url: Substring to be present in the URL.
+        """
         self.wait_for(EC.url_contains(url))
 
-    def await_presence(self, locator):
+    def await_presence(self, locator: tuple[By, str]) -> Union[WebElement, list[WebElement]]:
+        """
+        Wait until the presence of an element located by the given locator.
+
+        :param locator: Locator tuple (By, value) to find the element.
+        :return: The WebElement once it is located.
+        """
         return self.wait_for(EC.presence_of_element_located(locator))
 
-    def await_pageload(self):
-        self.wait_for(lambda d: d.execute_script('return document.readyState') == 'complete')
+    def await_pageload(self) -> None:
+        """
+        Wait until the page has completely loaded.
+        """
+        self.wait_for(
+            lambda d: d.execute_script("return document.readyState") == "complete"
+        )
 
-    def wait_for(self, condition, timeout=10):
+    def wait_for(self, condition: Callable[[WebDriver], T], timeout: int = 10) -> T:
+        """
+        Wait for a specific condition to be met within the given timeout.
+
+        :param condition: A callable that takes a WebDriver instance and returns a value of type T.
+        :param timeout: Maximum time to wait for the condition to be met.
+        :return: The result of the condition callable.
+        """
         return WebDriverWait(self.driver, timeout).until(condition)
